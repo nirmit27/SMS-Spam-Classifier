477905fe6e4c56d6c647b22dd2c847d8d7f52ab4
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-10-26 22:35:21 +0530

    API for generating labels

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SMS Spam Classifier
 
-This is an SMS spam classification web application built using the **Flask** framework.
+This is an SMS spam classification API built using the **Flask** framework.
+
+It processes SMS messages to determine whether they are `ham` or `spam`, providing an efficient, backend solution for automated message filtering.
--- app.py
+++ app.py
@@ -1,18 +1,36 @@
-from flask import Flask, jsonify, render_template, request, url_for
+from pickle import load
+from flask import Flask, jsonify, request, url_for, render_template
 
-app: Flask = Flask(__name__)
+app = Flask(__name__)
+model = load(open(r'rfc-model.pkl', 'rb'))
+vectorizer = load(open(r'tfidf-vectorizer.pkl', 'rb'))
 
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template('index.html', title='Home')
+    return render_template('index.html', title="SMS Classifier")
 
 
-@app.route("/test", methods=["POST"])
-def test():
+@app.route("/api/label", methods=["POST"])
+def message_label():
     data = request.get_json()
+    vector = vectorizer.transform([data['message']]).toarray()
+    prediction = model.predict(vector.reshape(1, -1))[0]
+
+    data.update({"label": "HAM" if prediction == 0 else "SPAM"})
     return jsonify(data), 200
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html', error=error.description, title="Not Found")
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('error.html', error=error.description, title="Internal Server Error")
+
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    with app.app_context():
+        app.run(debug=True, port=5000)
